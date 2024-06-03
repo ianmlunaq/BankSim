a6cfd8dce4c95443ac2f976692b06c62afb1f939
Ian Luna Quiroz <ianmlunaq@gmail.com>
2024-06-02 20:19:07 -0700

    Implemented initial basic pages and navigation

---
+++ src/main/java/banksimulator/Help.java
@@ -0,0 +1,4 @@
+package banksimulator;
+
+public class Help {
+}
---
+++ src/main/java/banksimulator/MenuMain.java
@@ -0,0 +1,39 @@
+package banksimulator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MenuMain {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToSceneMenuManager(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/MenuManager.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToSceneMenuCustomer(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/MenuCustomer.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToSceneHelp(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/Help.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
